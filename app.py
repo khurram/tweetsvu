45de3289323096ddc326d3a6a674ab7ac15c2d08
Khurram Aslam <khurram@aslam.me>
2011-12-16 03:26:20 +0000

    added linking

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from twython import Twython
+from twitter_text import Autolink
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,7 +11,14 @@ def index():
 @app.route('/report')
 def report():
     return render_template('report.html')
+
+@app.route('/search')
+def search():
+    query = request.args.get('q')
+    twitter = Twython()
+    results = twitter.searchTwitter(q=query)
+    return render_template('search.html', results=results, Autolink=Autolink)
     
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
