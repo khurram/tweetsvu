59fb5e0586c1c9417c72a6389e18a4a0d2e46926
Khurram Aslam <khurram@aslam.me>
2011-12-16 02:00:36 +0000

    updated static content

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "hello"
+def index():
+    return render_template('index.html')
 
+@app.route('/report')
+def report():
+    return render_template('report.html')
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
