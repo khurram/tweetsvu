caa4815a9978500997370cafaff791c104bf8458
Khurram Aslam <khurram@aslam.me>
2012-04-24 17:10:15 +0100

    switching to heroku

--- app.py
+++ app.py
@@ -77,4 +77,5 @@ def search():
     return render_template('search.html', params=params)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
